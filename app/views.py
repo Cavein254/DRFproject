81c10ec846597ec7d9ba45b14866fe579236fecd
johnmuthua <caveomuthama@gmail.com>
2024-01-24 22:56:36 +0300

    chore: cleared typos

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.views import APIViews
+from rest_framework.views import APIView
 from .serializers import *
 from .models import *
 from rest_framework.response import Response
--- project/urls.py
+++ project/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url 
+from django.urls import path, re_path
+
 from app.views import *
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', ReactView.as_view(), name="react")
+    re_path('admin/', admin.site.urls),
+    re_path('', ReactView.as_view(), name="react")
 ]
